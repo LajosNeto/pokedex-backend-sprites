cf7ab40e398a0ea467d6a97d63140fb39d11df6a
Lajos Onodi Neto <lajosnetogit@gmail.com>
2021-03-25 09:39:45 -0300

    Update sprites naming pattern
    Update sprites files naming pattern from id to pokemon name.
    Update filter script to work with new sprites file naming pattern.

--- filter.py
+++ filter.py
@@ -8,9 +8,12 @@ Pokemon sprites filter
 import argparse
 import os
 from shutil import copyfile
+import requests
 
 FILE_EXTENSION = ".png"
 BASE_FOLDER = "original-source/"
+BASE_API_URL = "https://pokeapi.co/api/v2/pokemon/"
+
 KANTO_FOLDER = "kanto-pokedex/"
 KANTO_IDS = range(1, 152)
 
@@ -18,9 +21,11 @@ def filter_kanto():
     if not os.path.exists(KANTO_FOLDER):
         os.makedirs(KANTO_FOLDER)
         for id in KANTO_IDS:
+            pokemon_data = requests.get(BASE_API_URL + str(id))
+            pokemon_data = dict(pokemon_data.json())
             copyfile(
                 BASE_FOLDER + str(id) + FILE_EXTENSION, 
-                KANTO_FOLDER + str(id) + FILE_EXTENSION)
+                KANTO_FOLDER + pokemon_data['name'] + FILE_EXTENSION)
     else :
         print("Kanto folder already exists. Maybe it's not empty?")
     
