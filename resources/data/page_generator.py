44ea4a616a4279a2269b90253ac79f179fb4597f
Lajos Neto <lajosnetogit@gmail.com>
2021-05-10 16:18:21 -0400

    Add new typesColors field to json

--- resources/data/page_generator.py
+++ resources/data/page_generator.py
@@ -13,13 +13,37 @@ import requests
 import json
 
 PAGES_BASE_PATH = "pages/page_"
+
 BASE_POKEMON_DETAILS_API_URL = "https://pokeapi.co/api/v2/pokemon/"
 BASE_NAMES_API_URL = "https://pokeapi.co/api/v2/pokemon-species/"
+
 BASE_SPRITE_PATH = "https://raw.githubusercontent.com/LajosNeto/pokedex-backend-sprites/main/resources/sprites/original-source/"
 SPRITE_FILE_TYPE = ".png"
 
 ALL_REGIONS_ID_RANGE = range(1, 899)
 PAGE_SIZE = 20
+
+TYPES_COLORS = {
+    "normal": "#9e9e6f",
+    "fire": "#ed7835",
+    "fighting": "#b72f29",
+    "water": "#5f84ea",
+    "flying": "#9e85eb",
+    "grass": "#6dc04c",
+    "poison": "#953a93",
+    "electric": "#f6cb3b",
+    "ground": "#dbb963",
+    "psychic": "#f6527e",
+    "rock": "#af9539",
+    "ice": "#8ed2d2",
+    "bug": "#9daf2b",
+    "dragon": "#6630f3",
+    "ghost": "#654e8c",
+    "dark": "#644e40",
+    "steel": "#afafc9",
+    "fairy": "#eb90a4",
+    "???": "#5e9585"
+}
 
 def build_pages():
     pokemons = list()
@@ -41,6 +65,7 @@ def build_pages():
         pokemon_data['nameJa'] = nameJa
         pokemon_data['sprite'] = BASE_SPRITE_PATH + str(id) + SPRITE_FILE_TYPE
         pokemon_data['types'] = [type['type']['name'] for type in pokemon_details['types']]
+        pokemon_data['typesColors'] = [TYPES_COLORS[type['type']['name']] for type in pokemon_details['types']]
 
         pokemons.append(pokemon_data)
     
